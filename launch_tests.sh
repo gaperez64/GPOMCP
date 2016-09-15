d631e22843d27098e0eae385a610ff2c685cbd67
Guillermo Pérez <gaperez@ServerVerif.local>
2016-09-15 11:03:36 +0200

    more data for tigerex and hallway

--- launch_tests.sh
+++ launch_tests.sh
@@ -2,7 +2,7 @@
 TIGEREX="../Examples/toytigerex.POMDP"
 LOG="log_tigerex.txt"
 HORIZON=100
-for i in `seq 0 28`;
+for i in `seq 0 50`;
 do
   THRESH=$(echo "${i} * 0.5" | bc)
   echo "threshold = "${THRESH} >> ${LOG}
--- launch_tests_hallway1_traps.sh
+++ launch_tests_hallway1_traps.sh
@@ -2,9 +2,9 @@
 TIGEREX="../Examples/Hallway/hallway1-traps.POMDP"
 LOG="log_hallway1_traps.txt"
 HORIZON=300
-for i in `seq 3500 50 4550`;
+for i in `seq 3000 50 5000`;
 do
   THRESH=${i}
   echo "threshold = "${THRESH} >> ${LOG}
-  ./test-sim ${TIGEREX} ${THRESH} ${HORIZON} 50 >> ${LOG}
+  ./test-sim ${TIGEREX} ${THRESH} ${HORIZON} 80 >> ${LOG}
 done
